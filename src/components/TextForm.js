3e6c88855f5e1bdc66796d334ff0d8e63b76c053
abhishek950650 <abhishek950650@gmail.com>
2023-07-24 21:31:47 +0530

    update

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -10,18 +10,46 @@ export default function TextForm(props) {
         let newText = event.target.value;
         setText(newText);
     });
-    const handleonClick = (() => {
+    const handleUpperonClick = (() => {
         let upText = text.toUpperCase();
         setText(upText);
     })
+    const handleLoweronClick = (() => {
+        let upText = text.toLowerCase();
+        setText(upText);
+    })
+
+    const handleCopyonClick = (() => {
+        let txt = document.getElementById('mytextBox');
+        txt.select();
+        navigator.clipboard.writeText(txt.value);
+    })
+
+    const handleRemoveSpaceonClick = (() => {
+        let txt = text.split(/[ ]+/);
+        setText(txt.join(" "))
+    })
     return (
+       <>
         <div>
             <div className="mb-3">
                 <h1>{props.heading}</h1>
-                <textarea className="form-control" value={text} onChange={handleonChange} id="mytextBox" rows="8"></textarea>
-                <button className="btn btn-primary" onClick={handleonClick}>Convert Into UpperCase</button>
+                <textarea className="form-control my-3" value={text} onChange={handleonChange} id="mytextBox" rows="8"></textarea>
+                <button className="btn btn-primary mx-1" onClick={handleUpperonClick}>Convert Into UpperCase</button>
+                <button className="btn btn-primary mx-1" onClick={handleLoweronClick}>Convert Into LowerCase</button>
+                <button className="btn btn-primary mx-1" onClick={handleCopyonClick}>Copy Text</button>
+                <button className="btn btn-primary mx-1" onClick={handleRemoveSpaceonClick}>Remove Extra Space</button>
             </div>
         </div>
+
+        <div className="container">
+            <h2>Your text summary</h2>
+            <p>{text.split(' ').length -1} word and {text.length}</p>
+            <p>{0.008* text.split(' ').length} minutes</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+       </>
     )
 }
 
